416e0a07769bd4806a6599e1de70b647c2d8bdbe
Vani Dawar <vani.dawar07@gmail.com>
2026-02-14 22:00:54 +0530

    Add files via upload

---
+++ 100days13.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main() 
+{
+    int r,c;
+    printf("Enter rows and columns");
+    scanf("%d %d",&r,&c);
+    int matrix[r][c];
+    for(int i=0;i<r;i++) 
+    {
+        for(int j=0;j<c;j++) 
+        {
+            printf("Enter element");
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    int top=0,bottom=r-1;
+    int left=0,right=c-1;
+    while(top<=bottom && left<=right) 
+    {
+        for(int j=left;j<=right;j++)
+        {
+            printf("%d",matrix[top][j]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++)
+        {
+            printf("%d",matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom) 
+        {
+            for (int j=right;j>=left;j--)
+            {
+                printf("%d",matrix[bottom][j]);
+            }
+            bottom--;
+        }
+        if(left<=right) 
+        {
+            for (int i=bottom;i>=top;i--)
+            {
+                printf("%d",matrix[i][left]);
+            }
+            left++;
+        }
+    }
+}

ff52bf9a86f7ade43defa98a07e6bdff497c450c
Vani Dawar <vani.dawar07@gmail.com>
2026-03-18 19:51:39 +0530

    Add files via upload

---
+++ 100days33.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <ctype.h>
+
+char stack[100];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int precedence(char op) {
+    if(op == '+' || op == '-') return 1;
+    if(op == '*' || op == '/') return 2;
+    return 0;  // FIXED
+}
+
+int main() {
+    char infix[100], postfix[100];
+    int k = 0;
+
+    scanf("%s", infix);
+
+    for(int i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+
+        if(isalnum(ch)) {
+            postfix[k++] = ch;
+        }
+        else if(ch == '(') {
+            push(ch);
+        }
+        else if(ch == ')') {
+            while(top != -1 && peek() != '(') {
+                postfix[k++] = pop();
+            }
+            pop(); // remove '('
+        }
+        else {
+            while(top != -1 && precedence(peek()) >= precedence(ch)) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while(top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}

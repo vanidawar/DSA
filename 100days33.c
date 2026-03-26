f63da6bc3d3e4fb22ba622e42f056b4f01a15e4f
Vani Dawar <vani.dawar07@gmail.com>
2026-03-26 20:52:13 +0530

    Add files via upload

--- 100days33.c
+++ 100days33.c
@@ -1,63 +1,48 @@
 #include <stdio.h>
 #include <ctype.h>
-
 char stack[100];
 int top = -1;
-
-void push(char x) {
-    stack[++top] = x;
+void push(char x){
+    stack[++top]=x;
 }
-
-char pop() {
+char pop(){
     return stack[top--];
 }
-
-char peek() {
+char peek(){
     return stack[top];
 }
-
-int precedence(char op) {
-    if(op == '+' || op == '-') return 1;
-    if(op == '*' || op == '/') return 2;
-    return 0;  // FIXED
+int precedence(char op){
+    if(op=='+' || op=='-') return 1;
+    if(op=='*' || op=='/') return 2;
 }
-
 int main() {
-    char infix[100], postfix[100];
-    int k = 0;
-
-    scanf("%s", infix);
-
-    for(int i = 0; infix[i] != '\0'; i++) {
-        char ch = infix[i];
-
+    char infix[100],postfix[100];
+    int k=0;
+    scanf("%s",infix);
+    for(int i=0;infix[i]!='\0';i++) {
+        char ch=infix[i];
         if(isalnum(ch)) {
-            postfix[k++] = ch;
+            postfix[k++]=ch;
         }
-        else if(ch == '(') {
+        else if(ch == '('){
             push(ch);
         }
-        else if(ch == ')') {
-            while(top != -1 && peek() != '(') {
-                postfix[k++] = pop();
+        else if(ch == ')'){
+            while(top!=-1 && peek()!='(') {
+                postfix[k++]=pop();
             }
-            pop(); // remove '('
+            pop();
         }
         else {
-            while(top != -1 && precedence(peek()) >= precedence(ch)) {
-                postfix[k++] = pop();
+            while(top!=-1 && precedence(peek())>=precedence(ch)) {
+                postfix[k++]=pop();
             }
             push(ch);
         }
     }
-
-    while(top != -1) {
-        postfix[k++] = pop();
+    while(top!=-1) {
+        postfix[k++]=pop();
     }
-
-    postfix[k] = '\0';
-
-    printf("%s", postfix);
-
-    return 0;
+    postfix[k]='\0';
+    printf("%s",postfix);
 }
